1caa0c5246f1c65fd0ffd275e67c8056c50790d5
Alexander <dell-95@mail.ru>
2016-12-12 21:03:48 +0300

    Add new activities and objects
    1) New activity: Take a weapon(bug: you can take more than one weapon at
    the same time)
    2) New activity: Turn On/Off lights
    3) Add new weapons to the project

---
+++ Assets/Scripts/LightSwitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSwitch : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+	public void SwitchOn() {
+		if (!(transform.FindChild ("fl_candle_light").gameObject.activeInHierarchy)) {
+			transform.FindChild ("fl_candle_light").gameObject.SetActive (true);
+		} else {
+			transform.FindChild("fl_candle_light").gameObject.SetActive(false);
+		}
+	}
+		
+}
---
+++ Assets/Scripts/TakeWeapon.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TakeWeapon : MonoBehaviour {
+
+	public GameObject SelectObject;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Take() {
+		if (gameObject.GetComponent<MeshRenderer>().enabled == true) {
+			gameObject.GetComponent<MeshRenderer>().enabled = false;
+			SelectObject.SetActive (true);
+		} else {
+			gameObject.GetComponent<MeshRenderer>().enabled = true;
+			SelectObject.SetActive (false);
+		}
+	}
+}
